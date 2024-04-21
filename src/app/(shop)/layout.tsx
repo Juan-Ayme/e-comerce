88d7e2c5867779b1f6d862ccfecc83a6d793aaf3
Juan Carlos Ayme <juanayme565@gmail.com>
2024-04-20 21:51:39 -0500

    componente title

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -3,9 +3,11 @@ import { TopMenu } from "@/components";
 export default function ShopLayout({children}:
     {children: React.ReactNode;}) {
   return (
-    <main className="min-h-screen ">
+    <main className="min-h-screen">
       <TopMenu/>
+      <div className="px-1 sm:px-5">
         {children}
+      </div>
     </main>
   );
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,13 @@
+import { Title } from "@/components";
 import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <div >
-      <h1>Hola mundo</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
-      <h1 className={`${titleFont.className}`}>Hola mundo</h1>
-    </div>
+    <>
+      <Title
+        title="Tienda"
+        subtitle="Todos los productos"
+      />
+    </>
   );
 }
