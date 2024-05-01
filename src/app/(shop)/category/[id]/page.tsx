5da75ec387d821fd51a09b8440ba113035d15cae
Juan Carlos Ayme <juanayme565@gmail.com>
2024-05-01 10:27:52 -0500

    Agregando filtro v1

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,11 @@
+'use client'
 import { ProductGrid, Title } from "@/components";
+import { Disclosure } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import { Category, ValidType } from "@/interfaces";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+import { useState } from "react";
 
 //el props es para recibir los parametros de la url
 
@@ -17,9 +21,10 @@ export default function AdminPage({params}:Props){
 
   const {id} = params;
   //const selectType:ValidType = 'shirts';// tipo de producto
-  const products = seedProducts.filter(product => product.gender === id);// filtra los productos por genero
-  //console.log(products);
-  //console.log(selectType);
+  const [selectTypes, setSelectTypes] = useState<ValidType[]>([]); // Cambia a un array
+  const products = seedProducts.filter(product => product.gender === id && (selectTypes.length === 0 || selectTypes.includes(product.type))); // Ajusta la lógica de filtrado
+  console.log(products);
+  console.log(selectTypes);
 
   const titles:Record<Category,string> = {
     'men':'Hombres',
@@ -36,6 +41,11 @@ export default function AdminPage({params}:Props){
   if(id!== 'men' && id !=='women'){
     notFound();
   }
+  const handleTypeChange = (type: ValidType) => { // Ajusta la lógica de manejo de cambios
+    setSelectTypes(prevTypes =>
+      prevTypes.includes(type) ? prevTypes.filter(t => t !== type) : [...prevTypes, type]
+    );
+  };
 
   return (
     <>
@@ -43,6 +53,35 @@ export default function AdminPage({params}:Props){
         title={`Articulos de ${titles[id]}`}// se obtendra el genero de acuerdo al id como llave
         subtitle={subtitles[id]}
       />
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-black bg-white rounded-lg hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <span>Filtrar por tipo</span>
+              <ChevronDownIcon className={`${open ? 'transform rotate-180' : ''} w-5 h-5 text-black`}/>
+            </Disclosure.Button>
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-gray-500">
+              <div>
+                <input type="checkbox" id="pants" name="type" value="pants" checked={selectTypes.includes('pants')} onChange={() => handleTypeChange('pants')} />
+                <label htmlFor="pants">Pantalones</label>
+              </div>
+              <div>
+                <input type="checkbox" id="shirts" name="type" value="shirts" checked={selectTypes.includes('shirts')} onChange={() => handleTypeChange('shirts')} />
+                <label htmlFor="shirts">Camisas</label>
+              </div>
+              <div>
+                <input type="checkbox" id="hoodies" name="type" value="hoodies" checked={selectTypes.includes('hoodies')} onChange={() => handleTypeChange('hoodies')} />
+                <label htmlFor="hoodies">Sudaderas</label>
+              </div>
+              <div>
+                <input type="checkbox" id="hats" name="type" value="hats" checked={selectTypes.includes('hats')} onChange={() => handleTypeChange('hats')} />
+                <label htmlFor="hats">Sombreros</label>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+
       <ProductGrid
         products={products}
       />
