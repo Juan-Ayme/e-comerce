69cf518328b8c124f57bb24570f0f4ccde676d1d
Juan Carlos Ayme <juanayme565@gmail.com>
2024-05-05 20:04:55 -0500

    cambio de estilos en talla

--- src/components/product/size-selector/SizeSelector.tsx
+++ src/components/product/size-selector/SizeSelector.tsx
@@ -1,6 +1,7 @@
+'use client'
 import { Size } from '@/interfaces'
 import clsx from 'clsx';
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props{
     selectedSize: Size;
@@ -8,6 +9,7 @@ interface Props{
 }
 
 export const SizeSelector = ({selectedSize,availableSizes}:Props) => {
+    const [clickedSize, setClickedSize] = useState(selectedSize);
 
     return (
         <div className='my-5'>
@@ -17,13 +19,14 @@ export const SizeSelector = ({selectedSize,availableSizes}:Props) => {
                 availableSizes.map(size => (
                     <button
                     key={size}
+                    onClick={() => setClickedSize(size)}
                     className = {
                         clsx(
-                            "mx-2 text-lg cursor-pointer transition-colors duration-200 ease-in-out rounded-full py-2 px-4",
-                            {
-                              'bg-blue-600 text-white hover:bg-blue-700': size === selectedSize,
-                              'bg-white border border-gray-300 text-gray-500 hover:bg-gray-200': size !== selectedSize
-                            }
+                            "mx-1 text-lg transition-colors duration-200 ease-in-out rounded-full py-2 px-4",
+                                {
+                                'bg-blue-600 text-white hover:bg-blue-700': size === clickedSize, //Si es la talla seleccionada
+                                'bg-gray-400 border border-gray-400 text-white hover:bg-blue-700': clickedSize !== size //Si no es la talla seleccionada
+                                }
                         )
                       }>
                         {size}
